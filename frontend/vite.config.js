b7e1b53e706e77f2d929d5705a2f6912e8591ca8
ketannemade5-ui <ketannemade5@gmail.com>
2026-09-07 19:35:09 +0530

    Deployment fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     host: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://kkn-traders-luhd.onrender.com/',
         changeOrigin: true,
       },
     },
